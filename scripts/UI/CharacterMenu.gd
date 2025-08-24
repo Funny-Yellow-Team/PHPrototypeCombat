5e78ab43ed2940db955cd0dc55e388653810c394
Adam Akkari <adam.akkari@epitech.eu>
2025-08-25 00:27:39 +0200

    Added border UI when character is selected

--- scripts/UI/CharacterMenu.gd
+++ scripts/UI/CharacterMenu.gd
@@ -1,4 +1,4 @@
-extends VBoxContainer
+extends PanelContainer
 class_name CharacterMenu
 
 @export var character: Node3D
@@ -11,9 +11,12 @@ var trg_buttons: Array[Button]
 var selected_targets: Array[Node3D]
 var input_name: String
 
+var not_selected_style: StyleBox = preload("res://assets/UI/not_selected_character_style.tres")
+var selected_style: StyleBox = preload("res://assets/UI/selected_character_style.tres")
+
 func _ready() -> void:
-	$TitleContainer/CharacterLabel.text = character.name
-	$CharacterHealthBar.set_character(character)
+	$CharacterContainer/TitleContainer/CharacterLabel.text = character.name
+	$CharacterContainer/CharacterHealthBar.set_character(character)
 	atk_cmp = ServiceContainer.get_attack_component(character)
 	atk_cmp.attack_done.connect(on_attack_done)
 	InputManager.input_mode_changed.connect(on_input_mode_changed)
@@ -25,13 +28,17 @@ func _ready() -> void:
 				InputManager.attack_inputs[i],
 				input_name)
 			btn.attack_button_pressed.connect(on_attack_button_pressed)
-			$AttackContainer.add_child(btn.get_parent())
+			$CharacterContainer/AttackContainer.add_child(btn.get_parent())
 			atk_buttons.append(btn)
 
 func _process(delta: float) -> void:
+	if (Input.is_action_pressed(input_name)):
+		add_theme_stylebox_override("panel", selected_style)
+	else:
+		add_theme_stylebox_override("panel", not_selected_style)
 	set_attack_button_disabled(!atk_cmp.can_attack)
 	if (atk_cmp.current_attack != null):
-		$CooldownBar.value = atk_cmp.current_attack.cooldown_timer.time_left
+		$CharacterContainer/CooldownBar.value = atk_cmp.current_attack.cooldown_timer.time_left
 
 func set_attack_button_disabled(disabled: bool):
 	for i in range(0, 3):
@@ -53,8 +60,8 @@ func on_attack_button_pressed(preset: AttackPreset):
 		else:
 			for ennemy in ennemies:
 				bind_target_button(ennemy)
-		$AttackContainer.visible = false
-		$TargetContainer.visible = true
+		$CharacterContainer/AttackContainer.visible = false
+		$CharacterContainer/TargetContainer.visible = true
 
 func on_target_button_pressed(target: Node3D):
 	selected_targets = [target]
@@ -65,13 +72,13 @@ func launch_attack():
 	reset_targets()
 
 func on_attack_done():
-	$CooldownBar.max_value = atk_cmp.current_attack.cooldown
-	$CooldownBar.value = atk_cmp.current_attack.cooldown
+	$CharacterContainer/CooldownBar.max_value = atk_cmp.current_attack.cooldown
+	$CharacterContainer/CooldownBar.value = atk_cmp.current_attack.cooldown
 
 func bind_target_button(selected_character: Node3D):
 	var btn = TargetButton.instantiate_button(selected_character)
 	btn.target_button_pressed.connect(on_target_button_pressed)
-	$TargetContainer.add_child(btn)
+	$CharacterContainer/TargetContainer.add_child(btn)
 	trg_buttons.append(btn)
 
 func _on_back_button_pressed() -> void:
@@ -80,15 +87,15 @@ func _on_back_button_pressed() -> void:
 func reset_targets():
 	selected_attack = null
 	selected_targets = []
-	$AttackContainer.visible = true
-	$TargetContainer.visible = false
+	$CharacterContainer/AttackContainer.visible = true
+	$CharacterContainer/TargetContainer.visible = false
 	for btn in trg_buttons:
 		btn.queue_free()
 	trg_buttons = []
 
 func set_input_name(new_input_name: String):
 	input_name = new_input_name
-	$TitleContainer/CharacterInputTexture.texture = InputManager.texture[input_name]
+	$CharacterContainer/TitleContainer/CharacterInputTexture.texture = InputManager.texture[input_name]
 
 func on_input_mode_changed():
-	$TitleContainer/CharacterInputTexture.texture = InputManager.texture[input_name]
+	$CharacterContainer/TitleContainer/CharacterInputTexture.texture = InputManager.texture[input_name]
